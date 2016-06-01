2f34e0b73a472816e4916a87553e342c5a5e0a9b
Krzysztof Szarlej <kszarlej94@gmail.com>
2016-06-02 00:03:01 +0200

    fix unnecessary newlines and comma

--- app/run.py
+++ app/run.py
@@ -8,8 +8,7 @@ def get_app():
 def start_app():
     get_app().run(host=app.config['HOST'],
                   port=app.config['PORT'],
-                  debug=app.config['DEBUG'],
-                  )
+                  debug=app.config['DEBUG'])
 
 if __name__ == '__main__':
     start_app()
