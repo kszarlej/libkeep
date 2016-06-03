1e8963f327eb438679acf192d9ef724eeaf4fb7f
Krzysztof Szarlej <kszarlej94@gmail.com>
2016-06-03 21:48:13 +0200

    add generate_slug listener and event

--- app/project/author/controllers.py
+++ app/project/author/controllers.py
@@ -5,15 +5,21 @@ from project.db import db, Author, Book
 from project.utils.auth import require_admin
 from .parsers import AuthorAddParser, AuthorParser
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy import event
 from project.utils.status import return_error, return_ok
 
 class List(Resource):
+
+    def slug_listener(mapper, connection, target):
+        target.slug = '{}-{}'.format(target.name, target.surname)
 
     def get(self, user_data):
         return [a.json for a in Author.query.all()]
 
     @require_admin
     def post(self, user_data):
+
+
         parser = AuthorAddParser(bundle_errors=True)
         args = parser.parse_args()
 
@@ -21,7 +27,6 @@ class List(Resource):
         author.name = args['name']
         author.surname = args['surname']
         author.www = args['www']
-        author.slug = "{0}-{1}".format(args['name'].lower(), args['surname'].lower())
 
         db.session.add(author)
 
@@ -32,11 +37,14 @@ class List(Resource):
         else:
             return return_ok()
 
+    event.listen(Author, 'before_insert', slug_listener)
+
+
 class Return(Resource):
 
     @require_admin
     def delete(self, id, user_data):
-        
+
         author = Author.query.filter_by(id=id).first()
         db.session.delete(author)
 
