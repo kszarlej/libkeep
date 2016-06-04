ec7a635f3d00ae66bf803f62ff7c860197fbecd8
Krzysztof Szarlej <kszarlej94@gmail.com>
2016-06-04 11:02:08 +0200

    fixes

--- app/project/app.py
+++ app/project/app.py
@@ -4,10 +4,12 @@ from project.utils import admin
 
 from .db import db
 
+
 def create_admin():
     with app.app_context():
         admin.create_admin(admin_email=app.config['ADMIN_EMAIL'],
                            admin_password=app.config['ADMIN_PASSWORD'])
+
 
 def create_tables():
     with app.app_context():
